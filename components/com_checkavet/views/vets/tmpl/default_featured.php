521d35e6acb0ab23687333f3004b51d0a6149a72
adam.c <adamdama@hotmail.com>
2012-05-27 19:54:00 +0100

    Added featured ordering to the vets front end view

---
+++ components/com_checkavet/views/vets/tmpl/default_featured.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @version     $Id: default.php 21576 2011-06-19 16:14:23Z chdemko $
+ * @package     Joomla.Administrator
+ * @subpackage  com_checkavet
+ * @copyright   Copyright (C) 2005 - 2011 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// No direct access.
+defined('_JEXEC') or die;
+
+include('default_vet.php');
