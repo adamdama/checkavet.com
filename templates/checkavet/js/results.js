b2faacf3281ed24fb12de53fed05b3b24355c05f
adam.c <adamdama@hotmail.com>
2012-07-05 23:56:19 +0100

    Added rollover and click functions to stars. click does nothing.

--- templates/checkavet/js/results.js
+++ templates/checkavet/js/results.js
@@ -1,8 +1,29 @@
 jQuery(document).ready(function(e) {
 	
+	var $this, form, img;
+	
 	jQuery('.star').click(function(e)
+	{		
+		//alert($this.index()+1);
+		form.find('input[name="user_rating"]').val($this.index()+1);
+	})
+	.mouseover(function(e)
 	{
+		$this = jQuery(this);
+		form = jQuery(this).parents('form');
+		img = $this.prevAll().children(':first-child').add($this.children(':first-child'));
+		img.attr('src', img.attr('src').replace('blank_', ''));
+	})
+	.mouseout(function(e)
+	{
+		var src, str = '';
+		src = img.attr('src').split('/');
 		
+		while(src.length > 1)
+			str +=	src.shift() + '/';
+						
+		str += 'blank_' + src.shift();
+		img.attr('src', str);
 	});
 
 });
