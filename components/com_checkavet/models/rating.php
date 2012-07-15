7e2844b80587c0d0f8c7825283b6b3d21ed88a7f
adam.c <adamdama@hotmail.com>
2012-07-15 17:19:21 +0100

    Added rating view to front end

---
+++ components/com_checkavet/models/rating.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @version		$Id: search.php 22338 2011-11-04 17:24:53Z github_bot $
+ * @package		Joomla.Site
+ * @subpackage	com_search
+ * @copyright	Copyright (C) 2005 - 2011 Open Source Matters, Inc. All rights reserved.
+ * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// No direct access
+defined('_JEXEC') or die;
+
+jimport('joomla.application.component.model');
+
+/**
+ * Search Component Search Model
+ *
+ * @package		Joomla.Site
+ * @subpackage	com_search
+ * @since 1.5
+ */
+class CheckavetModelRating extends JModel
+{
+	
+}
---
+++ components/com_checkavet/views/rating/tmpl/default.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @version		$Id: default.php 21576 2011-06-19 16:14:23Z chdemko $
+ * @package		Joomla.Administrator
+ * @subpackage	com_checkavet
+ * @copyright	Copyright (C) 2005 - 2011 Open Source Matters, Inc. All rights reserved.
+ * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// No direct access.
+defined('_JEXEC') or die;
+
+require_once(JPATH_COMPONENT.'/helpers/html.php');
+
+JFactory::getDocument()->addStyleSheet('templates/checkavet/shadowbox/shadowbox.css')
+						->addScript('templates/checkavet/shadowbox/shadowbox.js');
+
+?>
+<div id="content">
+	XXXX
+</div>
